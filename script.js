d9b63673d67443d16d128e2517ef42b6ba14ddfe
AhmadNrdn20 <ahmadnrdnsah@gmail.com>
2020-12-13 10:21:51 -0800

    my mini project

---
+++ script.js
@@ -0,0 +1,34 @@
+function playdo(){
+    let audio = document.getElementById('do_audio');
+    audio.play();
+}
+
+function playre(){
+    let audio = document.getElementById('re_audio');
+    audio.play();
+}
+
+function playmi(){
+    let audio = document.getElementById('mi_audio');
+    audio.play();
+}
+
+function playfa(){
+    let audio = document.getElementById('fa_audio');
+    audio.play();
+}
+
+function playsol(){
+    let audio = document.getElementById('sol_audio');
+    audio.play();
+}
+
+function playla(){
+    let audio = document.getElementById('la_audio');
+    audio.play();
+}
+
+function playsi(){
+    let audio = document.getElementById('si_audio');
+    audio.play();
+}
